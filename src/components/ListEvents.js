3bf716fc36d5d33085a023b7d4663ede1b48d37c
janbarend66 <72501763+janbarend66@users.noreply.github.com>
2021-04-30 18:07:09 +0200

    eventlijst kan een plaatje toegevoegd worden

--- src/components/ListEvents.js
+++ src/components/ListEvents.js
@@ -2,6 +2,7 @@ const ListEvents = ({post}) => {
     return (
         <>
                 <div className={"eventItem"}>
+                    {post.fimg_url ? <img src={post.fimg_url}/> : ""}
             <h3>{post.title.rendered}</h3><div className={"eventItemString"}>{post.cf.app_field_datum} - {post.cf.app_field_begintijd}:00 t/m {post.cf.app_field_eindtijd}:00</div>
             <div dangerouslySetInnerHTML={{__html: post.content.rendered}}></div>
                 </div>
