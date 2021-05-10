450cc1e8229a1b45afcc25c4bce49a1cc8e2dc1b
janbarend66 <72501763+janbarend66@users.noreply.github.com>
2021-05-10 14:45:47 +0200

    test

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -4,6 +4,7 @@ import {AiFillStar} from "react-icons/ai";
 import {useEffect} from "react"
 
 const SideBar = ({titles, posts}) => {
+    console.log("sidebar")
     function myFunction(x) {
         if (x.matches) { // If media query matches
             document.querySelector(".sideNav").classList.remove("visible")
