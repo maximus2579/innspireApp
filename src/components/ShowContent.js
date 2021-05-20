f455d21a21e010e6651011f3f2d5ffb4b395f4a9
janbarend66 <72501763+janbarend66@users.noreply.github.com>
2021-05-20 13:42:27 +0200

    update ls integration

--- src/components/ShowContent.js
+++ src/components/ShowContent.js
@@ -35,6 +35,7 @@ const ShowContent = ({posts}) => {
             console.log(document.querySelector("#iconmap").children)
             for (let i = 0; i<document.querySelector("#iconmap").children.length; i++){
                 document.querySelector("#iconmap").children[i].href = "/" + params.param + document.querySelector("#iconmap").children[i].pathname.slice(0, -1).trim()
+                document.querySelector("#iconmap").children[i].target = "_blank";
                 console.log(document.querySelector("#iconmap").children[i].href)
             }
         }
