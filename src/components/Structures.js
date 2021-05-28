0d0e13022b40e78510a932bf75c8c682a81133a6
janbarend66 <72501763+janbarend66@users.noreply.github.com>
2021-05-28 10:56:23 +0200

    ls integration

--- src/components/Structures.js
+++ src/components/Structures.js
@@ -1,7 +1,10 @@
 const Structures = ({posts}) => {
-    function getStructure (id){
+    function getStructure (e, id){
         for (let i=0; i<document.getElementsByClassName("structure").length; i++) {
+            console.log(document.querySelectorAll("#navStructures > div")[i])
+            document.querySelectorAll("#navStructures > div")[i].classList.remove("structureActive")
             document.getElementsByClassName("structure")[i].classList.add("hide")
+                e.target.classList.add("structureActive")
             if (parseInt(document.getElementsByClassName("structure")[i].attributes[0].value) === id){
                 document.getElementsByClassName("structure")[i].classList.remove("hide")
                 document.getElementsByClassName("structure")[i].classList.add("visible")
@@ -14,7 +17,7 @@ const Structures = ({posts}) => {
         <div className={"contentSection"}>
             <div className={"postContent"}>
                 <div id="navStructures">
-                    {posts.map((post) => <div onClick={() => getStructure(post.id)}>{post.title.rendered}</div>)}
+                    {posts.map((post) => <div onClick={(e) => getStructure(e, post.id)}>{post.title.rendered}</div>)}
                 </div>
                 <hr className={"devider"}/>
                     {posts.map((post) => <div data-id={post.id} className={"structure hide"} dangerouslySetInnerHTML={{__html: post.content.rendered}}></div>)}
