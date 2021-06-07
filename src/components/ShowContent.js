300265e1c3188af918c33dfbe49e8531b98f40fa
janbarend66 <72501763+janbarend66@users.noreply.github.com>
2021-06-07 14:33:20 +0200

    test

--- src/components/ShowContent.js
+++ src/components/ShowContent.js
@@ -31,7 +31,7 @@ const ShowContent = ({posts, titleID, titleIDChildren}) => {
         if (document.querySelector("#navStructures")){
             var newPosts = posts.filter(post => post.categories[0] === titleID)
             for (let i = 0; i<titleIDChildren.length; i++){
-                newPosts.push(posts.filter(post => post.categories[0] === titleIDChildren[i])[0])
+                newPosts.concat(posts.filter(post => post.categories[0] === titleIDChildren[i]))
             }
             console.log(newPosts)
             var tableCells = [];
